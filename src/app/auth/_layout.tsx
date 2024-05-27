19e9e193c0501812c0fbe7588384533739e249e2
danteBenitez <benitezdante123@gmail.com>
2024-05-27 14:19:30 -0300

    fix: Hide app header for auth screens

--- src/app/auth/_layout.tsx
+++ src/app/auth/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-    return <Stack screenOptions={{
-        headerShown: false
-    }}/>
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
