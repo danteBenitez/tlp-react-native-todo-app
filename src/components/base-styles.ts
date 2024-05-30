81d67e0aca11486433a28ee486db140f77a542a5
Dante Benitez <benitezdante123@gmail.com>
2024-05-29 22:44:52 -0300

    refactor: Common styles

---
+++ src/components/base-styles.ts
@@ -0,0 +1,19 @@
+import { ScaledSheet } from "react-native-size-matters";
+
+export const baseStyles = ScaledSheet.create({
+    baseText: {
+        height: "auto",
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        lineHeight: 18,
+    },
+    heading: {
+        fontWeight: 800,
+        fontSize: 30,
+    },
+    subheading: {
+        marginTop: "20@s",
+        fontWeight: 400,
+        fontSize: 20,
+    },
+})
