2c58a9bfd12892ccf4b3e1b6357e12baac8b52c9
Dante Benitez <benitezdante123@gmail.com>
2024-05-31 22:43:04 -0300

    refactor: Move task form component out

--- src/app/(app)/add-task.tsx
+++ src/app/(app)/add-task.tsx
@@ -1,133 +1,21 @@
 import { baseStyles } from "@/components/base-styles";
 import InputController from "@/components/input-controller";
-import MarginLayout from "@/components/tasks/margin-layout";
-import { taskSchema } from "@/components/tasks/schema";
-import { useAuth } from "@/hooks/use-auth";
+import TaskForm from "@/components/tasks/task-form";
 import { useTasks } from "@/hooks/use-tasks";
 import { Task } from "@/interfaces/task";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { router } from "expo-router";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { ScrollView, View } from "react-native";
-import { Button, List, Text, useTheme } from "react-native-paper";
-import {
-  DatePickerModal,
-  es,
-  registerTranslation,
-} from "react-native-paper-dates";
-
-registerTranslation("es", es);
 
 export default function AddTodo() {
-  const [visible, setVisible] = useState(false);
-  const { user } = useAuth()!;
-  const { control, handleSubmit, getValues, setValue, getFieldState } = useForm<Task>({
-    defaultValues: {
-      title: "",
-      description: "",
-      author: user?.username,
-      date: new Date(),
-    },
-    resolver: zodResolver(taskSchema),
-  });
-  const { addTask } = useTasks()!;
-  const theme = useTheme();
+  const { addTask } = useTasks();
+  const handleSubmit = (fields: Task) => {
+    addTask(fields);
+  };
   return (
-    <MarginLayout>
-      <View
-        style={{
-          padding: 10,
-          flex: 1,
-        }}
-      >
-        <Text
-          variant="titleLarge"
-          style={[baseStyles.baseText, baseStyles.heading]}
-        >
-          Crea una nueva tarea
-        </Text>
-        <Text
-          variant="displayLarge"
-          style={[
-            baseStyles.baseText,
-            baseStyles.subheading,
-            {
-              lineHeight: 30,
-            },
-          ]}
-        >
-          Agrega una nueva tarea a tu lista para no olvidar nada.
-        </Text>
-        <ScrollView
-          contentContainerStyle={{
-            marginTop: 38,
-            paddingHorizontal: 10,
-            height: "95%",
-            justifyContent: "flex-start",
-          }}
-        >
-          <InputController
-            name="title"
-            control={control}
-            inputProps={{
-              mode: "outlined",
-              label: "Título",
-            }}
-          />
-          <InputController
-            name="description"
-            control={control}
-            inputProps={{
-              style: {
-                height: 100,
-              },
-              multiline: true,
-              mode: "outlined",
-              label: "Descripción",
-              textAlignVertical: "bottom",
-              textAlign: "left",
-            }}
-          />
-          <Button mode="outlined" contentStyle={{ justifyContent: "flex-start" }} icon="calendar" style={{
-            marginTop: 30,
-            paddingVertical: 10,
-            borderRadius: 10 
-          }} onPress={() => setVisible(true)}>
-            {getFieldState("date").isDirty ? getValues("date")?.toLocaleDateString() : "Selecciona una fecha"}
-          </Button>
-          <DatePickerModal
-            onDismiss={() => setVisible(false)}
-            date={getValues("date") ?? new Date()}
-            visible={visible}
-            locale="es"
-            label="Fecha de la tarea"
-            onConfirm={({ date }) => {
-              setValue("date", date ?? new Date(), {
-                shouldDirty: true,
-              });
-              setVisible(false);
-            }}
-            mode={"single"}
-          />
-          <Button
-            mode="outlined"
-            style={{
-              marginTop: "auto",
-              marginBottom: 50,
-              paddingHorizontal: 10,
-              paddingVertical: 3,
-              width: "100%",
-            }}
-            onPress={handleSubmit((fields) => {
-              addTask(fields);
-              router.replace("/(app)");
-            })}
-          >
-            Crear tarea
-          </Button>
-        </ScrollView>
-      </View>
-    </MarginLayout>
+    <TaskForm
+      titleText="Agrega una nueva tarea" 
+      subtitleText="Crea tareas para nunca olvidar nada"
+      onSubmit={handleSubmit} 
+      getValues={() => undefined}
+      buttonText="Agregar tarea"
+    /> 
   );
 }
---
+++ src/components/tasks/task-form.tsx
@@ -0,0 +1,165 @@
+import { useAuth } from "@/hooks/use-auth";
+import { Task } from "@/interfaces/task";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import { taskSchema } from "./schema";
+import { Button, Text } from "react-native-paper";
+import MarginLayout from "./margin-layout";
+import { View, ScrollView } from "react-native";
+import { baseStyles } from "../base-styles";
+import InputController from "../input-controller";
+import { router } from "expo-router";
+import {
+  DatePickerModal,
+  es,
+  registerTranslation,
+} from "react-native-paper-dates";
+
+registerTranslation("es", es);
+
+type TaskFormProps = {
+  titleText: string;
+  subtitleText: string;
+  buttonText: string;
+  onSubmit: (task: Task) => void;
+  getValues: () => Task | undefined;
+};
+
+export default function TaskForm(props: TaskFormProps) {
+  const [visible, setVisible] = useState(false);
+  const { user } = useAuth()!;
+  const form = useForm<Task>({
+    defaultValues: {
+      title: "",
+      description: "",
+      author: user?.username,
+      date: new Date(),
+    },
+    shouldUnregister: false,
+    resolver: zodResolver(taskSchema),
+  });
+
+  const { control, watch, handleSubmit, getValues, setValue, getFieldState } = form;
+
+  useEffect(() => {
+    console.log("cambiando valores por defecto", props.getValues());
+    const task = props.getValues();
+    if (task) {
+      console.log(task);
+      setValue("title", task.title, { shouldDirty: true });
+      setValue("description", task.description, { shouldDirty: true });
+      setValue("date", task.date, { shouldDirty: true });
+    }
+  }, [props.getValues]);
+
+  return (
+    <FormProvider {...form}>
+      <MarginLayout>
+        <View
+          style={{
+            padding: 10,
+            flex: 1,
+          }}
+        >
+          <Text
+            variant="titleLarge"
+            style={[baseStyles.baseText, baseStyles.heading]}
+          >
+            {props.titleText}
+          </Text>
+          <Text
+            variant="displayLarge"
+            style={[
+              baseStyles.baseText,
+              baseStyles.subheading,
+              {
+                lineHeight: 30,
+              },
+            ]}
+          >
+            {props.subtitleText}
+          </Text>
+          <ScrollView
+            contentContainerStyle={{
+              marginTop: 38,
+              paddingHorizontal: 10,
+              height: "95%",
+              justifyContent: "flex-start",
+            }}
+          >
+            <InputController
+              name={"title"}
+              control={control}
+              inputProps={{
+                value: watch('title'),
+                mode: "outlined",
+                label: "Título",
+              }}
+            />
+            <InputController
+              name="description"
+              control={control}
+              inputProps={{
+                style: {
+                  height: 100,
+                },
+                multiline: true,
+                value: watch('description'),
+                mode: "outlined",
+                label: "Descripción",
+                textAlignVertical: "bottom",
+                textAlign: "left",
+              }}
+            />
+            <Button
+              mode="outlined"
+              contentStyle={{ justifyContent: "flex-start" }}
+              icon="calendar"
+              style={{
+                marginTop: 30,
+                paddingVertical: 10,
+                borderRadius: 10,
+              }}
+              onPress={() => setVisible(true)}
+            >
+              {getFieldState("date").isDirty
+                ? watch('date')?.toLocaleDateString()
+                : "Selecciona una fecha"}
+            </Button>
+            <DatePickerModal
+              onDismiss={() => setVisible(false)}
+              date={watch('date') ?? new Date()}
+              visible={visible}
+              locale="es"
+              label="Fecha de la tarea"
+              onConfirm={({ date }) => {
+                setValue("date", date ?? new Date(), {
+                  shouldDirty: true,
+                });
+                setVisible(false);
+              }}
+              mode={"single"}
+            />
+            <Button
+              mode="outlined"
+              style={{
+                marginTop: "auto",
+                marginBottom: 50,
+                paddingHorizontal: 10,
+                paddingVertical: 3,
+                width: "100%",
+              }}
+              onPress={handleSubmit((fields) => {
+                props.onSubmit(fields);
+                router.replace("/(app)");
+              })}
+            >
+              {props.buttonText}
+            </Button>
+          </ScrollView>
+        </View>
+      </MarginLayout>
+    </FormProvider>
+  );
+}
