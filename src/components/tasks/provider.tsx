321509d002ba92581e0dc491a49cf0d9d627ccef
Dante Benitez <benitezdante123@gmail.com>
2024-05-31 23:27:12 -0300

    feat: Move example tasks to JSON file

--- src/components/tasks/provider.tsx
+++ src/components/tasks/provider.tsx
@@ -1,4 +1,5 @@
 import { Task } from "@/interfaces/task";
+import { Serialized } from "@/interfaces/serialized";
 import { ReactNode, createContext, useState } from "react";
 
 type TaskContextValue = {
@@ -8,50 +9,12 @@ type TaskContextValue = {
   editTask: (taskId: number, newTask: Task) => void;
 };
 
-const DEFAULT_TASKS: Task[] = [
-  {
-    id: 1,
-    title: "Comprar pan",
-    description: "Panadería la flor de la vida",
-    author: "Yo",
-    date: new Date(),
-  },
-  {
-    id: 2,
-    title: "Llamar a mamá",
-    description: "Recordarle que le quiero",
-    author: "Yo",
-    date: new Date(),
-  },
-  {
-    id: 3,
-    title: "Reunión con el jefe",
-    description: "Presentar informe de ventas",
-    author: "Yo",
-    date: new Date(),
-  },
-  {
-    id: 4,
-    title: "Comprar pan",
-    description: "Panadería la flor de la vida",
-    author: "Yo",
-    date: new Date(),
-  },
-  {
-    id: 5,
-    title: "Llamar a mamá",
-    description: "Recordarle que le quiero",
-    author: "Yo",
-    date: new Date(),
-  },
-  {
-    id: 6,
-    title: "Reunión con el jefe",
-    description: "Presentar informe de ventas",
-    author: "Yo",
-    date: new Date(),
-  },
-];
+const DEFAULT_TASKS: Task[] = require('./tasks.json').map((json: Serialized<Task>) => {
+  return {
+    ...json,
+    date: new Date(json.date),
+  }
+});
 
 export const TaskContext = createContext<TaskContextValue | null>(null);
 
